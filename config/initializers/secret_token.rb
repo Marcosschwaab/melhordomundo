cd9ab1aba8fbcf0d3483a041c3e619b2fa747dce
Marcos Schwaab <marcosschwaab@AL-QAEDA.local>
2011-11-17 11:24:59 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Melhordomundo::Application.config.secret_token = '4fbb2e1314231364570d17b3afdd85ed999bb490cd967897dbb1a9c4b5cb8fcb6589158d5be00f38f880ebc82e163db06b719446c521b49cdc3b33a466db9c76'
